6f979ad2c13eece8c9f366b2048793b8de781632
Tim <16307130090@fudan.edu.cn>
2018-12-04 22:26:16 +0800

    Sub UI Layout

--- adview.py
+++ adview.py
@@ -19,45 +19,68 @@ class advance_view(QWidget):
         self.setAcceptDrops(True)
 
     def initUI(self):
-        self.sas_label = QLabel("SAS位置：",self)
-        self.sas_label.move(10,10)
+        self.vbox = QVBoxLayout() # MAIN box
+
+        self.sas_label = QLabel("SAS位置：")
+        # self.sas_label.move(10,10)
         self.sas_label.resize(104,30)
         self.sas_label.setFont(self.plain_text_font)
 
-        self.sas_input = QLineEdit(self.info_list[0],self)
+        self.vbox.addWidget(self.sas_label)
+
+        self.line2 = QHBoxLayout()
+        self.vbox.addLayout(self.line2)
+
+        self.sas_input = QLineEdit(self.info_list[0])
         self.sas_input.resize(233,30)
-        self.sas_input.move(10,40)
+        # self.sas_input.move(10,40)
         self.sas_input.setFont(self.plain_text_font)
+
+        self.line2.addWidget(self.sas_input)
         
-        self.browser_button = QPushButton("浏览",self)
+        self.browser_button = QPushButton("浏览")
         self.browser_button.resize(58,32)
         self.browser_button.setFont(self.plain_text_font)
-        self.browser_button.move(246,40)
+        # self.browser_button.move(246,40)
         self.browser_button.clicked.connect(self.browser_action)
 
-        self.total_time_label = QLabel("最大可接受运行时间",self)
-        self.total_time_label.move(10,80)
+        self.line2.addWidget(self.browser_button)
+
+        self.total_time_label = QLabel("最大可接受运行时间")
+        # self.total_time_label.move(10,80)
         self.total_time_label.resize(187,30)
         self.total_time_label.setFont(self.plain_text_font)
 
-        self.total_time_input = QLineEdit(str(self.info_list[1]),self)
-        self.total_time_input.move(10,110)
+        self.vbox.addWidget(self.total_time_label)
+
+        self.line4 = QHBoxLayout()
+        self.vbox.addLayout(self.line4)
+
+        self.total_time_input = QLineEdit(str(self.info_list[1]))
+        # self.total_time_input.move(10,110)
         self.total_time_input.resize(100,30)
 
-        self.confirm_button = QPushButton("确定",self)
-        self.confirm_button.move(140,110)
+        self.line4.addWidget(self.total_time_input)
+
+        self.confirm_button = QPushButton("确定")
+        # self.confirm_button.move(140,110)
         self.confirm_button.resize(68,32)
         self.confirm_button.clicked.connect(self.confirm_pushed)
         self.confirm_button.setFont(self.plain_text_font)
 
-        self.cancel_button = QPushButton("取消",self)
-        self.cancel_button.move(210,110)
+        self.line4.addWidget(self.confirm_button)
+
+        self.cancel_button = QPushButton("取消")
+        # self.cancel_button.move(210,110)
         self.cancel_button.resize(68,32)
         self.cancel_button.clicked.connect(self.cancel_pushed)
         self.cancel_button.setFont(self.plain_text_font)
 
+        self.line4.addWidget(self.cancel_button)
+
         self.setGeometry(536,465,300,165)
         self.setWindowTitle("高级设置")
+        self.setLayout(self.vbox)
         # self.show()
 
     def dragEnterEvent(self, e):
